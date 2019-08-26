f57f3de33bbd34d0e9c46fd957d38bfc15adad52
Camavilca <ocamavilcachavez@gmail.com>
2019-08-26 17:44:58 -0600

    fix

--- CLASES/clase03/App2/App2/view/ScrollViewDemoCode.xaml.cs
+++ CLASES/clase03/App2/App2/view/ScrollViewDemoCode.xaml.cs
@@ -9,15 +9,16 @@ using Xamarin.Forms.Xaml;
 
 namespace App2.view
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ScrollViewDemoCode : ContentPage
-	{
-		public ScrollViewDemoCode ()
-		{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScrollViewDemoCode : ContentPage
+    {
+        public ScrollViewDemoCode ()
+        {
             var scroll = new ScrollView();
             Content = scroll;
             var stack = new StackLayout();
-            stack.Children.Add(new BoxView { BackgroundColor = Color.Red, HeightRequest = 600, WidthRequest = 600 });
+            stack.Children.Add(new BoxView 
+                { BackgroundColor = Color.Red, HeightRequest = 600, WidthRequest = 600 });
             stack.Children.Add(new Entry());
             scroll.Content = stack;
             Content = scroll;
