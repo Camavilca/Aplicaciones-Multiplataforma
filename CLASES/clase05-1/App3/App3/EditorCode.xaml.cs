62fd48899320e46c44460dfb6b89b99e44c2e816
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-09-16 20:01:44 -0600

    fix

---
+++ CLASES/clase05-1/App3/App3/EditorCode.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorCode : ContentPage
+    {
+        public EditorCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var MyEditor = new Editor { Text = "I am an Editor" };
+            stack.Children.Add(MyEditor);
+            Content = stack;
+
+        }
+    }
+}
---
+++ CLASES/clase05-1/App3/App3/EntryCode.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryCode : ContentPage
+    {
+        public EntryCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var entry = new Entry { Text = "This is a read-only Entry" };
+            stack.Children.Add(entry);
+            Content = stack;
+
+        }
+    }
+}
